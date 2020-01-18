84cf4baddd88f1eabae427c7a88d2676b5dc06fe
Joseph Harburg <josephharburg@gmail.com>
2020-01-18 14:44:57 -0500

    added Form and validation

---
+++ CoffeeShop/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using CoffeeShop.Models;
+
+namespace CoffeeShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        //need one action to load registration page another view;
+        public IActionResult Register()
+        {
+
+            return View();
+        }
+        //need one action to take userInputs and display user name and whatever else you want and display a new view;
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult AddUser(UserID user)
+        {
+          
+
+            return View(user);
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+
+   
+}
---
+++ CoffeeShop/wwwroot/js/site.js
@@ -0,0 +1,20 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+//function validate() {
+ 
+//    if (document.querySelector('#pw1').value !== document.querySelector('#pw2').value) {
+        
+//        return true;
+        
+//    }
+//    else {
+//        document.querySelector("#pw2").placeholder = "Passwords Dont Match";
+//        return false;
+        
+//    }
+    
+//}
+//document.querySelector("#pw2").setCustomValidity("");
